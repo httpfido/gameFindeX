698ce5166b1386cd0564aea6da3b144683f69b29
FidoDido <fidel.romano.2016@hotmail.com>
2023-02-20 02:31:12 -0300

    axios default OK

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import {  Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-axios.defaults.baseURL = "pi-videogames-production-f655.up.railway.app"
+axios.defaults.baseURL = "https://pi-videogames-production-f655.up.railway.app"
 
 // import "./App.css";
 
