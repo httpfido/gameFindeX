39a674ce6ec1d5d41e863544a05d5011c4a11f36
FidoDido <fidel.romano.2016@hotmail.com>
2023-02-20 05:06:11 -0300

    footer not position fixed

--- client/src/App.js
+++ client/src/App.js
@@ -15,12 +15,13 @@ function App() {
   return (
     <div className="App">
       {location.pathname !== '/' && <NavBar/> }
-      {location.pathname !== '/' && <Footer/> }
 
       <Route exact path="/" render={()=><Landing/>}/>
       <Route exact path="/home" render={()=><Home/>}/>
       <Route exact path="/home/:id" render={()=><Detail/>}/>
       <Route exact path="/create" render={()=><Form/>}/>
+      
+      {location.pathname !== '/' && <Footer/> }
     </div>
   );
 }
