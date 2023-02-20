600b9611f35367606803205b90450cd27f78dc6d
FidoDido <fidel.romano.2016@hotmail.com>
2023-02-20 00:21:54 -0300

    to Deploy

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 
 import {  Route, useLocation } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-axios.defaults.baseURL = "http://localhost:3001/"
+axios.defaults.baseURL = "https://pi-videogames-production-f655.up.railway.app/"
 
 // import "./App.css";
 
